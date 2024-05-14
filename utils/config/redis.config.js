3f48075a3678daa445bcc84d9818136170c48495
Pradeep3238 <d.pradeep3238@gmail.com>
2024-05-14 23:25:42 +0530

    remote redis setup

--- utils/config/redis.config.js
+++ utils/config/redis.config.js
@@ -1,5 +1,6 @@
 import redis from 'redis';
-
+import dotenv from 'dotenv';
+dotenv.config({ path: ".env" });
 // Get the Redis URL from the environment variable
 const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
 
